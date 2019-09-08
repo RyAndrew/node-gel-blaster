b77084fdc2cb40ae611ee9b9fd18f23b252413fc
Andrew Ry <RyAndrew@users.noreply.github.com>
2019-09-08 03:55:10 -0500

    add throttle+steering, rename events

--- node-gel-blaster.js
+++ node-gel-blaster.js
@@ -15,14 +15,29 @@ const httpPort = 8080;
 // PCA9685 options
 var options = {
 	i2c: i2cBus.openSync(1),
-	address: 0x40,
+	//address: 0x40, // generic PCA9685
+	address: 0x60, // Adafruit Motor Driver
 	frequency: 50
 	//debug: true
 };
 
+var ServoMax = 2400;
+var ServoMin = 800;
+var ServoRange = ServoMax - ServoMin;
 
-var pwmPanChannel = 1;
-var pwmFireChannel = 14;
+var pwmPanChannel = 15;
+var pwmTiltChannel = 14;
+
+var steeringValue = 500; // midpoint - straight foward
+
+var throttleThreshold = 10.0
+var throttleMidpoint = 500.0
+
+var motorPwmMax = 4095
+var motorPwmRange = 1000.0
+var motorPwmOffset = motorPwmMax - motorPwmRange
+
+var pwmShootChannel = 1;
 
 var pwm = new pca9685.Pca9685Driver(options, function startLoop(err) {
 	if (err) {
@@ -33,10 +48,116 @@ var pwm = new pca9685.Pca9685Driver(options, function startLoop(err) {
 	console.log("PCA9685 Initialized");
 });
 
-pwm.setPulseLength(pwmPanChannel, 800);
+pwm.setPulseLength(pwmPanChannel, 1500);
+pwm.setPulseLength(pwmTiltChannel, 1500);
+
+// function sendPwm(channel, onStep, offStep){
+
+//     const channel0OnStepLowByte = 0x06; // LED0_ON_L
+//     const channel0OnStepHighByte = 0x07; // LED0_ON_H
+//     const channel0OffStepLowByte = 0x08; // LED0_OFF_L
+//     const channel0OffStepHighByte = 0x09; // LED0_OFF_H
+//     const registersPerChannel = 4;
+
+// 	pwm.send([
+// 		{ command: channel0OnStepLowByte + registersPerChannel * channel, byte: onStep & 0xFF },
+//         { command: channel0OnStepHighByte + registersPerChannel * channel, byte: (onStep >> 8) & 0x0F },
+//         { command: channel0OffStepLowByte + registersPerChannel * channel, byte: offStep & 0xFF },
+//         { command: channel0OffStepHighByte + registersPerChannel * channel, byte: (offStep >> 8) & 0x0F }
+// 	], 
+// 	() => { return; }
+// 	);
+
+// }
 
 
+// function motorSetPercent(motorNo, direction, throttlePercent){
+// 	let pinPwm, pinin1, pinin2;
 
+// 	switch (motorNo) {
+// 		default:
+// 			console.log("invalid motor! "+ motorNo);
+// 			return;
+// 		case 1:
+// 			pinPwm = 2;
+// 			pinin1 = 4;
+// 			pinin2 = 3;
+// 			break;
+// 		case 2:
+// 			pinPwm = 7;
+// 			pinin1 = 6;
+// 			pinin2 = 5;
+// 			break;
+// 	}
+
+// 	var onValue = 4096;
+
+// 	if (throttlePercent == 0) {
+// 		sendPwm(pinPwm, 0, onValue);
+// 		sendPwm(pinin1, 0, onValue);
+// 		sendPwm(pinin2, 0, onValue);
+// 	}else{
+
+// 		if (direction >= 1) {
+// 			//pwm.setPulseLength(pinin1, 0, onValue);
+// 			//pwm.setPulseLength(pinin2, onValue, 0);
+// 			sendPwm(pinin1, 0, onValue);
+// 			sendPwm(pinin2, onValue, 0);
+// 		} else {
+// 			sendPwm(pinin1, onValue, 0);
+// 			sendPwm(pinin2, 0, onValue);
+// 		}
+
+// 		var throttleValue = motorPwmOffset + (motorPwmRange*throttlePercent);
+// 		console.log("motor "+motorNo+" throttle pwm value" +throttleValue);
+
+// 		sendPwm(pinPwm, 0, throttleValue);
+// 		//pwm.setPulseLength(pinPwm, 0, throttleValue);
+// 	}
+// }
+
+
+function motorSetPercent(motorNo, direction, throttlePercent){
+	let pinPwm, pinin1, pinin2;
+
+	switch (motorNo) {
+		default:
+			console.log("invalid motor! "+ motorNo);
+			return;
+		case 1:
+			pinPwm = 2;
+			pinin1 = 4;
+			pinin2 = 3;
+			break;
+		case 2:
+			pinPwm = 7;
+			pinin1 = 6;
+			pinin2 = 5;
+			break;
+	}
+
+	var onValue = 4095;
+
+	if (throttlePercent == 0) {
+		pwm.setPulseLength(pinPwm, 0, onValue);
+		pwm.setPulseLength(pinin1, 0, onValue);
+		pwm.setPulseLength(pinin2, 0, onValue);
+	}else{
+
+		if (direction >= 1) {
+			pwm.setPulseLength(pinin1, 0, onValue);
+			pwm.setPulseLength(pinin2, onValue, 0);
+		} else {
+			pwm.setPulseLength(pinin1, onValue, 0);
+			pwm.setPulseLength(pinin2, 0, onValue);
+		}
+
+		var throttleValue = motorPwmOffset + (motorPwmRange*throttlePercent);
+		console.log("motor "+motorNo+" throttle pwm value" +throttleValue);
+
+		pwm.setPulseLength(pinPwm, 0, throttleValue);
+	}
+}
 
 const webSocketServer = new WebSocket.Server({ noServer: true });
 
@@ -61,30 +182,91 @@ webSocketServer.on('connection', function connection(ws, req) {
 			return;
 		}
 		switch(messageJson.action){
-			case "updateSteering":
-				if(!messageJson.value){
+			case "setShoot":
+				if(!messageJson.hasOwnProperty('value')){
 					console.log('invalid json message, missing value');
 					return;
 				}
-				let pwmValue = Math.round(800 + (1400 * (parseInt(messageJson.value)/1000)));
-				console.log('update steering = '+messageJson.value+' / pwm '+pwmValue);
 				
-				pwm.setPulseLength(pwmPanChannel, pwmValue);
-
-				break;
-				
-			case "updateThrottle":
-				if(!messageJson.value){
-					console.log('invalid json message, missing value');
-					return;
-				}
-				if(messageJson.value >= 960){
+				if(messageJson.value >= 1){
 					console.log("FIRE!");
-					pwm.setPulseLength(pwmFireChannel, 4096);
+					pwm.setPulseLength(pwmShootChannel, 4096);
 				}else{
 					console.log("STOP FIRE!");
-					pwm.setPulseLength(pwmFireChannel, 0);
+					pwm.setPulseLength(pwmShootChannel, 0);
 				}
+
+				break;
+			case "setSteering":
+				if(!messageJson.hasOwnProperty('value')){
+					console.log('invalid json message, missing value');
+					return;
+				}
+				steeringValue = parseInt(messageJson.value);
+
+				break;
+			case "setThrottle":
+				if(!messageJson.hasOwnProperty('value')){
+					console.log('invalid json message, missing value');
+					return;
+				}
+
+				var throttleValue = messageJson.value;
+				if (throttleValue > throttleMidpoint-throttleThreshold && throttleValue < throttleMidpoint+throttleThreshold) {
+
+					console.log("setThrottle stop "+throttleValue);
+
+					motorSetPercent(1, 0, 0);
+					motorSetPercent(2, 0, 0);
+
+				} else {
+
+					//calc steering offset
+
+					if (throttleValue >= 500) {
+						//forward
+						throttleValue = (throttleValue - 500) / 500;
+
+						console.log("setThrottle fwd throttleValue = "+throttleValue );
+
+						motorSetPercent(1, 1, throttleValue);
+						motorSetPercent(2, 1, throttleValue);
+
+					} else {
+						//reverse
+						throttleValue = (500 - throttleValue) / 500;
+
+						console.log("setThrottle rev throttleValue = "+throttleValue );
+
+						motorSetPercent(1, -1, throttleValue);
+						motorSetPercent(2, -1, throttleValue);
+					}
+				}
+
+				break;
+			case "setTilt":
+				if(!messageJson.hasOwnProperty('value')){
+					console.log('invalid json message, missing value');
+					return;
+				}
+				var servoPercent = messageJson.value / 1000;
+
+				var servoValue = ServoRange * servoPercent + ServoMin;
+
+				console.log('tilt pwm = '+servoValue);
+				pwm.setPulseLength(pwmTiltChannel, servoValue);
+				break;
+			case "setPan":
+				if(!messageJson.hasOwnProperty('value')){
+					console.log('invalid json message, missing value');
+					return;
+				}
+				var servoPercent = messageJson.value / 1000;
+
+				var servoValue = ServoRange * servoPercent + ServoMin;
+
+				console.log('pan pwm = '+servoValue);
+				pwm.setPulseLength(pwmPanChannel, servoValue);
 				break;
 			default:
 				console.log("invalid action!");
@@ -96,7 +278,7 @@ webSocketServer.on('connection', function connection(ws, req) {
 		console.log('connection closed for ' + ip);
 	});
 
-	ws.send('something');
+	ws.send('{"connected":1}');
 });
 
 
